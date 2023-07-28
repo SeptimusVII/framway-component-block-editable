7e7fcd100ced9f6d47c50ec0b751d89ad38a10e1
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-07-28 15:46:30 +0200

    bump version

--- block-editable.js
+++ block-editable.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var BlockEditable = Object.getPrototypeOf(app).BlockEditable = new app.Component("block-editable");
     //BlockEditable.debug = true;
     BlockEditable.createdAt      = "2.0.0";
-    BlockEditable.lastUpdate     = "2.0.0";
-    BlockEditable.version        = "1";
+    BlockEditable.lastUpdate     = "2.4.0";
+    BlockEditable.version        = "1.0.1";
     // BlockEditable.factoryExclude = true;
     // BlockEditable.loadingMsg     = "This message will display in the console when component will be loaded.";
     // BlockEditable.requires       = [];
